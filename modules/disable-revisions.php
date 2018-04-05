90bef74fa4b75b786031677949490bfea75bbe8b
Nathan Bolton <nathan@nathanbolton.ca>
2018-04-04 22:52:34 -0700

    Revision tweaks

---
+++ modules/disable-revisions.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Disables revisions for a smaller database size.
+ */
+
+define( 'WP_POST_REVISIONS', 0 );
--- modules/limit-revisions.php
+++ modules/limit-revisions.php
@@ -3,6 +3,4 @@
  * Limits revisions to 3 for a smaller database size.
  */
 
-namespace Think_Nathan\Abettor\LimitRevisions;
-
 define( 'WP_POST_REVISIONS', 3 );
