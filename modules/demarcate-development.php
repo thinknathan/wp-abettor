a279d7951790f100572257412d89e339d3bf684d
Nathan Bolton <nathan@nathanbolton.ca>
2018-04-09 23:16:29 -0700

    update formatting

--- modules/demarcate-development.php
+++ modules/demarcate-development.php
@@ -10,10 +10,33 @@ namespace Think_Nathan\Abettor\DemarcateDevelopment;
  * Adds a red box to the top-left of all pages with the word "DEV"
  */
 function add_development_styles() {
-  if (WP_ENV === 'development') {
-    $output = "<style>html::before { content: 'DEV'; font-weight: 700; letter-spacing: 1px; line-height: 3; text-align: center; width: 3rem; height: 3rem; background: red; display: block; position: fixed; top: 0; left: 0; z-index: 999999; pointer-events: none; opacity: .5; }</style>";
-    echo $output;
-  }
+  if (defined('WP_ENV') && WP_ENV === 'development'):
+  ?>
+    <style>
+      /**
+      * wp-abettor plugin
+      * Adds a red box to the top-left of all pages with the word "DEV" 
+      */
+      html::before {
+        content: 'DEV';
+        font-weight: 700;
+        letter-spacing: 1px;
+        line-height: 3;
+        text-align: center;
+        width: 3rem;
+        height: 3rem;
+        background: red;
+        display: block;
+        position: fixed;
+        top: 0;
+        left: 0;
+        z-index: 999999;
+        pointer-events: none;
+        opacity: .5;
+      }
+    </style>
+  <?php
+  endif;
 }
 add_action( 'wp_head',    __NAMESPACE__ . '\\add_development_styles', 5 );
 add_action( 'admin_head', __NAMESPACE__ . '\\add_development_styles', 5 );
@@ -25,30 +48,43 @@ add_action( 'admin_head', __NAMESPACE__ . '\\add_development_styles', 5 );
  * Credit: http://www.totallycommunications.com/latest/dynamic-favicons-step-by-step-guide/
  */
 function add_development_favicon() {
-  if (WP_ENV === 'development') {
-    $output = '<script>(function(c){';
-    $output .= 'var b=c.createElement("canvas"),d=c.createElement("img"),e=c.querySelector("link[rel=icon]");';
-    $output .= 'if (!e) {return};';
-    $output .= 'var f=e.cloneNode(!0);';
-    $output .= 'if("function"==typeof b.getContext){b.width=16;b.height=16;';
-    $output .= 'var a=b.getContext("2d");';
-    $output .= 'd.onload=function(){';
-    $output .= 'a.drawImage(this,0,0,16,16);';
-    $output .= 'a.font="bold 12px Arial";';
-    $output .= 'a.fillStyle="red";';
-    $output .= 'a.strokeStyle="white";';
-    $output .= 'a.lineWidth=2;';
-    $output .= 'a.textAlign="center";';
-    $output .= 'a.strokeText("D",8,13);';
-    $output .= 'a.fillText("D",8,13);';
-    $output .= 'f.href=b.toDataURL("image/png");';
-    $output .= 'c.head.appendChild(f)';
-    $output .= '};';
-    $output .= 'd.src=e.href';
-    $output .= '}})(document);';
-    $output .= '</script>';
-    echo $output;
-  }
+  if (defined('WP_ENV') && WP_ENV === 'development'):
+  ?>
+    <script>
+      /**
+      * wp-abettor plugin
+      * Adds the letter D on top of the existing favicon 
+      */
+      (function (c) {
+        var b = c.createElement("canvas"),
+          d = c.createElement("img"),
+          e = c.querySelector("link[rel=icon]");
+        if (!e) {
+          return;
+        };
+        var f = e.cloneNode(!0);
+        if ("function" == typeof b.getContext) {
+          b.width = 16;
+          b.height = 16;
+          var a = b.getContext("2d");
+          d.onload = function () {
+            a.drawImage(this, 0, 0, 16, 16);
+            a.font = "bold 12px Arial";
+            a.fillStyle = "red";
+            a.strokeStyle = "white";
+            a.lineWidth = 2;
+            a.textAlign = "center";
+            a.strokeText("D", 8, 13);
+            a.fillText("D", 8, 13);
+            f.href = b.toDataURL("image/png");
+            c.head.appendChild(f);
+          };
+          d.src = e.href;
+        };
+      })(document);
+    </script>
+  <?php
+  endif;
 }
 add_action( 'wp_footer',    __NAMESPACE__ . '\\add_development_favicon', 5 );
 add_action( 'admin_footer', __NAMESPACE__ . '\\add_development_favicon', 5 );
