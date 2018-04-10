b9d913aa67e24274e9f82a4030a72f9a2f7c164c
Nathan Bolton <nathan@nathanbolton.ca>
2018-04-09 23:16:18 -0700

    update formatting & apply only to large screens

--- modules/disable-backend-admin-bar.php
+++ modules/disable-backend-admin-bar.php
@@ -6,7 +6,28 @@
 namespace Think_Nathan\Abettor\DisableBackendAdminBar;
 
 function add_admin_styles() {
-  $output = "<style>#wpadminbar{display:none!important}html{padding-top:0!important}</style>";
-  echo $output;
+?>
+  <style>
+    /**
+    * wp-abettor plugin
+    * Hide admin bar for large screens
+    */
+    @media only screen and (min-width: 783px) {
+      #wpadminbar {
+        display: none !important;
+      }
+      html { 
+        padding-top: 0 !important;
+      }
+    }
+    
+    /**
+    * Hide WordPress icon on top menu
+    */
+    #wp-admin-bar-wp-logo {
+      display: none !important;
+    }
+  </style>
+<?php
 }
 add_action( 'admin_head', __NAMESPACE__ . '\\add_admin_styles', 6 );
